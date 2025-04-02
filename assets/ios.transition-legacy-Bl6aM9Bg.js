3c39027e8f186ebe5a6306d8e0876e90d8b54a23
keynaGogo1 <keynagogo5@gmail.com>
2025-04-02 17:01:09 +0800

    Updates

--- assets/ios.transition-legacy-Bl6aM9Bg.js
+++ assets/ios.transition-legacy-Bl6aM9Bg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DZRcQnF1.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-CBlhPrPF.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
