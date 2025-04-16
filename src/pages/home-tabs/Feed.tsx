46f96145ac40df7fda59fa4dcf293ffb9b809269
Ronabaldoza <20221238@nbsc.edu.ph>
2025-04-16 15:42:39 +0800

    added edit in Feed.tsx

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,10 +1,5 @@
 import { 
-  IonButtons,
-    IonCard,
-    IonCardContent,
-    IonCardHeader,
-    IonCardSubtitle,
-    IonCardTitle,
+   IonButtons,
     IonContent, 
     IonHeader, 
     IonMenuButton, 
@@ -12,6 +7,7 @@ import {
     IonTitle, 
     IonToolbar 
 } from '@ionic/react';
+import FeedContainer from '../../components/FeedContainer';
 
 const Feed: React.FC = () => {
   return (
@@ -25,73 +21,19 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-      <IonCard>
-    <IonCardHeader>
-      <IonCardTitle>Cute Birds</IonCardTitle>
-      <IonCardSubtitle></IonCardSubtitle>
-    </IonCardHeader>
-
-    <IonCardContent></IonCardContent>
-  </IonCard>
-  <IonCard>
-    <img alt="Silhouette of mountains" src="https://images.unsplash.com/photo-1444464666168-49d633b86797?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmlyZHxlbnwwfHwwfHx8MA%3D%3D" />
-    <IonCardHeader>
-      <IonCardTitle>Blue Kingfisher</IonCardTitle>
-      <IonCardSubtitle></IonCardSubtitle>
-    </IonCardHeader>
-
-    <IonCardContent></IonCardContent>
-  </IonCard>
-  <IonCard>
-    <img alt="Silhouette of mountains" src="https://plus.unsplash.com/premium_photo-1675714692342-01dfd2e6b6b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmlyZHxlbnwwfHwwfHx8MA%3D%3D" />
-    <IonCardHeader>
-      <IonCardTitle>Orange Vcut Bird</IonCardTitle>
-      <IonCardSubtitle></IonCardSubtitle>
-    </IonCardHeader>
-
-    <IonCardContent></IonCardContent>
-  </IonCard>
-  <IonCard>
-    <img alt="Silhouette of mountains" src="https://images.unsplash.com/photo-1606567595334-d39972c85dbe?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmlyZHxlbnwwfHwwfHx8MA%3D%3D" />
-    <IonCardHeader>
-      <IonCardTitle>Green Bird</IonCardTitle>
-      <IonCardSubtitle></IonCardSubtitle>
-    </IonCardHeader>
-
-    <IonCardContent></IonCardContent>
-  </IonCard>
-  <IonCard>
-    <img alt="Silhouette of mountains" src="https://images.unsplash.com/photo-1555169062-013468b47731?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmlyZHxlbnwwfHwwfHx8MA%3D%3D" />
-    <IonCardHeader>
-      <IonCardTitle>Orange Bird</IonCardTitle>
-      <IonCardSubtitle></IonCardSubtitle>
-    </IonCardHeader>
-
-    <IonCardContent></IonCardContent>
-  </IonCard>
-  <IonCard>
-    <img alt="Silhouette of mountains" src="https://images.unsplash.com/photo-1497206365907-f5e630693df0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJpcmR8ZW58MHx8MHx8fDA%3D" />
-    <IonCardHeader>
-      <IonCardTitle>Flamingo</IonCardTitle>
-      <IonCardSubtitle></IonCardSubtitle>
-    </IonCardHeader>
-
-    <IonCardContent></IonCardContent>
-  </IonCard>
-
-
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100%',
-        }}
-      >
-      </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Feed
+        </div>
+        <FeedContainer />
       </IonContent>
     </IonPage>
   );
 };
-
-export default Feed;
+export default Feed; 
